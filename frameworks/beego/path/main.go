20b860a16b7e0236a452ba6f6fe5f91fa7d701e6
冯立强 <fenglq@tingyun.com>
2021-06-17 16:04:27 +0800

    add beego

---
+++ frameworks/beego/path/main.go
@@ -0,0 +1,164 @@
+// Copyright 2021 冯立强 fenglq@tingyun.com.  All rights reserved.
+// +build linux
+// +build amd64
+
+package beegoframe
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"runtime"
+
+	"github.com/beego/beego/server/web/context"
+
+	"github.com/TingYunGo/goagent"
+	beego "github.com/beego/beego/server/web"
+	param "github.com/beego/beego/server/web/context/param"
+)
+
+const (
+	beegoRoutineLocalIndex = 9 + 8 + 8 + 8
+)
+
+type handlerInfo struct {
+	name   string
+	method string
+	isFunc bool
+}
+
+//go:noinline
+func beegoAddMethod(p *beego.ControllerRegister, method, pattern string, f beego.FilterFunc) {
+	fmt.Println(p, method, pattern, f)
+}
+
+var methodMap = map[string]string{
+	"GET":     "Get",
+	"POST":    "Post",
+	"PUT":     "Put",
+	"DELETE":  "Delete",
+	"PATCH":   "Patch",
+	"OPTIONS": "Options",
+	"HEAD":    "Head",
+	"TRACE":   "Trace",
+}
+
+//go:noinline
+func WrapbeegoAddMethod(p *beego.ControllerRegister, method, pattern string, f beego.FilterFunc) {
+	handlerPC := reflect.ValueOf(f).Pointer()
+	methodName := runtime.FuncForPC(handlerPC).Name()
+	info := handlerInfo{
+		name:   methodName,
+		isFunc: true,
+	}
+	tingyun3.LocalSet(beegoRoutineLocalIndex, info)
+	defer tingyun3.LocalDelete(beegoRoutineLocalIndex)
+	beegoAddMethod(p, method, pattern, f)
+}
+
+//go:noinline
+func beegoHandler(p *beego.ControllerRegister, pattern string, h http.Handler, options ...interface{}) {
+	fmt.Println(p, pattern, h, options)
+}
+
+//go:noinline
+func WrapbeegoHandler(p *beego.ControllerRegister, pattern string, h http.Handler, options ...interface{}) {
+	handlerPC := reflect.ValueOf(h).Pointer()
+	methodName := runtime.FuncForPC(handlerPC).Name()
+	info := handlerInfo{
+		name:   methodName,
+		isFunc: true,
+	}
+	if len(methodName) == 0 {
+		reflectVal := reflect.ValueOf(h)
+		info.name = reflect.Indirect(reflectVal).Type().String()
+		info.method = "ServeHTTP"
+		info.isFunc = false
+	}
+	tingyun3.LocalSet(beegoRoutineLocalIndex, info)
+	defer tingyun3.LocalDelete(beegoRoutineLocalIndex)
+	beegoHandler(p, pattern, h, options...)
+}
+
+//go:noinline
+func beegoaddWithMethodParams(p *beego.ControllerRegister, pattern string, c beego.ControllerInterface, methodParams []*param.MethodParam, mappingMethods ...string) {
+	fmt.Println(p, pattern, c, methodParams, mappingMethods)
+}
+
+//go:noinline
+func WrapbeegoaddWithMethodParams(p *beego.ControllerRegister, pattern string, c beego.ControllerInterface, methodParams []*param.MethodParam, mappingMethods ...string) {
+	reflectVal := reflect.ValueOf(c)
+	info := handlerInfo{
+		name:   reflect.Indirect(reflectVal).Type().String(),
+		isFunc: false,
+	}
+	tingyun3.LocalSet(beegoRoutineLocalIndex, info)
+	defer tingyun3.LocalDelete(beegoRoutineLocalIndex)
+	beegoaddWithMethodParams(p, pattern, c, methodParams, mappingMethods...)
+}
+
+//go:noinline
+func beegoAddAutoPrefix(p *beego.ControllerRegister, prefix string, c beego.ControllerInterface) {
+	fmt.Println(p, prefix, c)
+}
+
+//go:noinline
+func WrapbeegoAddAutoPrefix(p *beego.ControllerRegister, prefix string, c beego.ControllerInterface) {
+	reflectVal := reflect.ValueOf(c)
+	info := handlerInfo{
+		name:   reflect.Indirect(reflectVal).Type().String(),
+		isFunc: false,
+	}
+	tingyun3.LocalSet(beegoRoutineLocalIndex, info)
+	defer tingyun3.LocalDelete(beegoRoutineLocalIndex)
+	beegoAddAutoPrefix(p, prefix, c)
+}
+
+var routeMap map[string]bool = nil
+
+//go:noinline
+func beegoaddToRouter(p *beego.ControllerRegister, method, pattern string, r *beego.ControllerInfo) {
+	fmt.Println(p, method, pattern, r)
+}
+
+//go:noinline
+func WrapbeegoaddToRouter(p *beego.ControllerRegister, method, pattern string, r *beego.ControllerInfo) {
+	if routeMap == nil {
+		routeMap = make(map[string]bool)
+	}
+	info := tingyun3.LocalGet(beegoRoutineLocalIndex)
+	if _, found := routeMap[pattern]; !found {
+		routeMap[pattern] = true
+		handler := handlerInfo{}
+		if info != nil {
+			handler = info.(handlerInfo)
+		}
+		p.InsertFilter(pattern, beego.BeforeExec, func(ctx *context.Context) {
+			action := tingyun3.GetAction()
+			if action != nil {
+				if len(handler.name) > 0 {
+					if handler.isFunc {
+						action.SetName("ROUTE", handler.name)
+					} else {
+						if len(handler.method) > 0 {
+							action.SetName("Method", handler.method)
+						} else if method, found := methodMap[action.GetMethod()]; found {
+							action.SetName("Method", method)
+						}
+						action.SetName("CLASS", handler.name)
+					}
+				}
+				action.SetName("URI", pattern)
+			}
+		})
+	}
+	beegoaddToRouter(p, method, pattern, r)
+}
+
+func init() {
+	tingyun3.Register(reflect.ValueOf(WrapbeegoaddToRouter).Pointer())
+	tingyun3.Register(reflect.ValueOf(WrapbeegoAddMethod).Pointer())
+	tingyun3.Register(reflect.ValueOf(WrapbeegoHandler).Pointer())
+	tingyun3.Register(reflect.ValueOf(WrapbeegoAddAutoPrefix).Pointer())
+	tingyun3.Register(reflect.ValueOf(WrapbeegoaddWithMethodParams).Pointer())
+}
--- tingyun.c
+++ tingyun.c
@@ -13,6 +13,7 @@ extern const WrapItem* TingyunWraps() {
         {"net/http.(*ServeMux).Handle", ".", "ServerMuxHandle"},
         {"net/http.(*Server).Serve", ".", "ServerServe"},
         {"net/http.(*Client).do", ".", "HttpClientDo"},
+
         {"database/sql.Open", "/database.", "DBOpen"},
         {"database/sql.(*DB).Close", "/database.", "DBClose"},
         {"database/sql.(*Conn).PrepareContext", "/database.", "ConnPrepareContext"},
@@ -28,6 +29,7 @@ extern const WrapItem* TingyunWraps() {
         {"database/sql.(*Stmt).ExecContext", "/database.", "StmtExecContext"},
         {"database/sql.(*Stmt).Close", "/database.", "StmtClose"},
         {"database/sql.(*Rows).Close", "/database.", "RowsClose"},
+
         {"github.com/gin-gonic/gin.(*RouterGroup).handle", "/frameworks/gin.", "RouterGrouphandle"},
         {"github.com/labstack/echo.(*Echo).add", "/frameworks/echo.", "echoEchoadd"},
         {"github.com/labstack/echo.(*Echo).Add", "/frameworks/echo.", "echoEchoAdd"},
@@ -35,9 +37,35 @@ extern const WrapItem* TingyunWraps() {
         {"github.com/labstack/echo/v4.(*Echo).add", "/frameworks/echo/v4.", "echoEchoadd"},
         {"github.com/labstack/echo/v4.(*Echo).Add", "/frameworks/echo/v4.", "echoEchoAdd"},
         {"github.com/labstack/echo/v4.(*Router).Add", "/frameworks/echo/v4.", "echoRouterAdd"},
+        //go mod v2
+        {"github.com/beego/beego/v2/server/web.(*ControllerRegister).addToRouter", "/frameworks/beego/v2.", "beegoaddToRouter"},
+        {"github.com/beego/beego/v2/server/web.(*ControllerRegister).AddMethod", "/frameworks/beego/v2.", "beegoAddMethod"},
+        {"github.com/beego/beego/v2/server/web.(*ControllerRegister).Handler", "/frameworks/beego/v2.", "beegoHandler"},
+        {"github.com/beego/beego/v2/server/web.(*ControllerRegister).AddAutoPrefix", "/frameworks/beego/v2.", "beegoAddAutoPrefix"},
+        {"github.com/beego/beego/v2/server/web.(*ControllerRegister).addWithMethodParams", "/frameworks/beego/v2.", "beegoaddWithMethodParams"},
+        //go mod 
+        {"github.com/astaxie/beego.(*ControllerRegister).addToRouter", "/frameworks/beego.", "beegoaddToRouter"},
+        {"github.com/astaxie/beego.(*ControllerRegister).AddMethod", "/frameworks/beego.", "beegoAddMethod"},
+        {"github.com/astaxie/beego.(*ControllerRegister).Handler", "/frameworks/beego.", "beegoHandler"},
+        {"github.com/astaxie/beego.(*ControllerRegister).AddAutoPrefix", "/frameworks/beego.", "beegoAddAutoPrefix"},
+        {"github.com/astaxie/beego.(*ControllerRegister).addWithMethodParams", "/frameworks/beego.", "beegoaddWithMethodParams"},
+        //go path
+        {"github.com/beego/beego/server/web.(*ControllerRegister).addToRouter", "/frameworks/beego/path.", "beegoaddToRouter"},
+        {"github.com/beego/beego/server/web.(*ControllerRegister).AddMethod", "/frameworks/beego/path.", "beegoAddMethod"},
+        {"github.com/beego/beego/server/web.(*ControllerRegister).Handler", "/frameworks/beego/path.", "beegoHandler"},
+        {"github.com/beego/beego/server/web.(*ControllerRegister).AddAutoPrefix", "/frameworks/beego/path.", "beegoAddAutoPrefix"},
+        {"github.com/beego/beego/server/web.(*ControllerRegister).addWithMethodParams", "/frameworks/beego/path.", "beegoaddWithMethodParams"},
+        //go path astaxie
+        {"github.com/astaxie/beego/server/web.(*ControllerRegister).addToRouter", "/frameworks/beego/path/astaxie.", "beegoaddToRouter"},
+        {"github.com/astaxie/beego/server/web.(*ControllerRegister).AddMethod", "/frameworks/beego/path/astaxie.", "beegoAddMethod"},
+        {"github.com/astaxie/beego/server/web.(*ControllerRegister).Handler", "/frameworks/beego/path/astaxie.", "beegoHandler"},
+        {"github.com/astaxie/beego/server/web.(*ControllerRegister).AddAutoPrefix", "/frameworks/beego/path/astaxie.", "beegoAddAutoPrefix"},
+        {"github.com/astaxie/beego/server/web.(*ControllerRegister).addWithMethodParams", "/frameworks/beego/path/astaxie.", "beegoaddWithMethodParams"},
+
         {"github.com/gomodule/redigo/redis.DialContext", "/nosql/redigo.", "RedigoDialContext"},
         {"github.com/gomodule/redigo/redis.(*conn).Close", "/nosql/redigo.", "RedigoConnClose"},
         {"github.com/gomodule/redigo/redis.(*conn).DoWithTimeout", "/nosql/redigo.", "RedigoDoWithTimeout"},
+
         {"github.com/go-redis/redis.(*baseClient).process", "/nosql/go-redis.", "baseClientprocess"},
         {"github.com/go-redis/redis.(*baseClient).Process", "/nosql/go-redis.", "baseClientProcess"},
         {"github.com/go-redis/redis.(*baseClient).processPipeline", "/nosql/go-redis.", "baseClientprocessPipeline"},
@@ -45,6 +73,28 @@ extern const WrapItem* TingyunWraps() {
         {"github.com/go-redis/redis/v7.(*baseClient).processPipeline", "/nosql/go-redis/v7.", "baseClientprocessPipeline"},
         {"github.com/go-redis/redis/v8.(*baseClient).process", "/nosql/go-redis/v8.", "baseClientprocess"},
         {"github.com/go-redis/redis/v8.(*baseClient).processPipeline", "/nosql/go-redis/v8.", "baseClientprocessPipeline"},
+
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).BulkWrite", "/nosql/mongodb.", "mongodbBulkWrite"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).InsertOne", "/nosql/mongodb.", "mongodbInsertOne"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).InsertMany", "/nosql/mongodb.", "mongodbInsertMany"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).DeleteOne", "/nosql/mongodb.", "mongodbDeleteOne"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).DeleteMany", "/nosql/mongodb.", "mongodbDeleteMany"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).UpdateMany", "/nosql/mongodb.", "mongodbUpdateMany"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).UpdateByID", "/nosql/mongodb.", "mongodbUpdateByID"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).UpdateOne", "/nosql/mongodb.", "mongodbUpdateOne"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).ReplaceOne", "/nosql/mongodb.", "mongodbReplaceOne"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).CountDocuments", "/nosql/mongodb.", "mongodbCountDocuments"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).EstimatedDocumentCount", "/nosql/mongodb.", "mongodbEstimatedDocumentCount"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).Distinct", "/nosql/mongodb.", "mongodbDistinct"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).Find", "/nosql/mongodb.", "mongodbFind"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).FindOne", "/nosql/mongodb.", "mongodbFindOne"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).FindOneAndDelete", "/nosql/mongodb.", "mongodbFindOneAndDelete"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).FindOneAndReplace", "/nosql/mongodb.", "mongodbFindOneAndReplace"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).FindOneAndUpdate", "/nosql/mongodb.", "mongodbFindOneAndUpdate"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).Watch", "/nosql/mongodb.", "mongodbWatch"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Collection).Drop", "/nosql/mongodb.", "mongodbDrop"},
+        {"go.mongodb.org/mongo-driver/mongo.NewClient", "/nosql/mongodb.", "mongodbNewClient"},
+        {"go.mongodb.org/mongo-driver/mongo.(*Client).Disconnect", "/nosql/mongodb.", "mongodbDisconnect"},
         {0, 0, 0}
     };
     return targets;
